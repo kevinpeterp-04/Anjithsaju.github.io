369aa41973818007923f8bcac48b3f069e7412de
Anjithsaju <anjithsaju123@gmail.com>
2024-06-12 17:50:10 +0530

    hi

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const page1=document.getElementById("page1");
 const page2=document.getElementById("page2");
 const page3=document.getElementById("page3");
 const wrapper= document.querySelectorAll('.p2');
-
+const pg2=document.querySelector('.pg2');
+const split=document.getElementById("split");
 page2.style.display="none";
 page3.style.display="none";
 back.style.display="none";
@@ -16,12 +17,16 @@ bt1.addEventListener("click",trans2);
 bt2.addEventListener("click",trans2);
 back.addEventListener("click",backfn);
 bfood.addEventListener("click",trans3);
+split.addEventListener("click",trans1);
+console.log(wrapper);
+
 function trans2()
 {
     wrapper.forEach(element => {
-        
+        element.style.background="";
         element.classList.remove('active');
     });
+    pg2.classList.remove('active');
     page3.style.display="none";
     page2.style.display="";
     page1.style.display="none";
@@ -42,11 +47,12 @@ function trans1()
 function trans3()
 {
     bk=3;
+    bfood.style.background="white";
     wrapper.forEach(element => {
-        
+        element.style.background="white";
         element.classList.add('active');
     });
-    
+    pg2.classList.add('active');
     page3.style.display="";
     console.log("going to page 3");
 }
